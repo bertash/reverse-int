d3779d4a3e6f3cbda5b11c41fd387ca028e8f358
Bertash <y.bertash@gmail.com>
2020-10-07 21:14:22 +0300

    solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let positive = n;
+    let result = 0;
+  if (n < 0) {
+    positive *= (-1);
+    result = positive.toString().split('').reverse('').join('');
+
+  } else {
+    result = positive.toString().split('').reverse('').join('');
+  }
+  return +result;
 }
